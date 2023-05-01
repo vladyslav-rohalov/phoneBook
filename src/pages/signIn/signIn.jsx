10de6b693800e60e8879783cfcd092efb661ab01
Vladyslav Rohalov <domus.lg@gmail.com>
2023-05-01 09:19:11 +0200

    Added error checking on signIn
    - If user or password is wrong, notify will be shown

--- src/pages/signIn/signIn.jsx
+++ src/pages/signIn/signIn.jsx
@@ -1,4 +1,6 @@
 import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useAuth } from 'hooks/useAuth';
 import authOperations from 'Redux/auth/operations';
 import { Container } from '../../components/elements/backdropContainer/backdropContainer.styled';
 import { ContainerUtils } from '../../components/elements/utilsContainer/utilsContainer.styled';
@@ -6,9 +8,17 @@ import { Title } from 'components/elements/title/title.styled';
 import { Form, Label, Input } from '../../components/elements/form/form.styled';
 import ButtonCommon from 'components/elements/button/button';
 import LinkTo from '../../components/elements/linkTo/linkTo';
+import { Toaster } from 'react-hot-toast';
+import { notifyError } from 'helpers/notify';
 
 export default function SignIn() {
   const dispatch = useDispatch();
+
+  const { error } = useAuth();
+
+  useEffect(() => {
+    if (error !== null && error.status >= 400) notifyError(error.message);
+  }, [error]);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -27,6 +37,7 @@ export default function SignIn() {
   return (
     <Container>
       <ContainerUtils>
+        <Toaster />
         <Title>Sign In</Title>
         <Form onSubmit={handleSubmit}>
           <Label>
