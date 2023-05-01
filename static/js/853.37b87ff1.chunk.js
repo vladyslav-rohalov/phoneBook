55277fe8426fd172b06c49b9ab8cc8aa183df693
vladyslav-rohalov <113693621+vladyslav-rohalov@users.noreply.github.com>
2023-05-01 12:32:01 +0000

    Deploying to gh-pages from @ vladyslav-rohalov/phonebook@abab40d2e8e75cc0ea21ccc09edcee503e051104 🚀

--- static/js/853.37b87ff1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[853],{3853:function(t,n,o){o.d(n,{Imn:function(){return r}});var e=o(9983);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"}},{tag:"polyline",attr:{points:"22,6 12,13 2,6"}}]})(t)}}}]);
-//# sourceMappingURL=853.37b87ff1.chunk.js.map
